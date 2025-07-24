106dd739cd149f997edd1388a911e69c2dd01a40
ramachandrasahoo <ramachandra.webdev@gmail.com>
2025-07-24 21:15:34 +0530

    new problem

---
+++ index.js
@@ -0,0 +1,21 @@
+// Check if a number is positive, negative, or zero
+// Want to try that one?
+function checknum(num) {
+    if (num > 0){
+        return `${num} is positive`
+    }
+    else if (num < 0){
+        return `${num} is negative`
+    }
+    else if (num == 0){
+        return `it is ${num} `
+    }
+}
+
+console.log(checknum(1.4));
+
+
+
+
+
+
